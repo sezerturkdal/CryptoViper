a76cc7130bfd8f51fb6ef2b4d679bd5297b74cf7
Sezer Turkdal <sezer.turkdal@hotmail.com>
2023-01-10 22:36:36 +0300

    Interactor created

--- CryptoViper/Presenter.swift
+++ CryptoViper/Presenter.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+enum NetworkError : Error{
+    case NetworkFailed
+    case ParsingFailed
+}
+
+protocol AnyPresenter{
+    var router : AnyRouter?{get set}
+    var interactor : AnyInteractor?{get set}
+    var view : AnyView?{get set}
+    
+    func interactorDidDownloadCrypto(result:Result<[Crypto],Error>)
+}
+
+class CryptoPresenter : AnyPresenter {
+    var router: AnyRouter?
+    
+    var interactor: AnyInteractor?
+    
+    var view: AnyView?
+    
+    func interactorDidDownloadCrypto(result: Result<[Crypto], Error>) {
+        switch result{
+        case .success(let cryptos):
+            print("update")
+        case .failure(let error):
+            print("error")
+        }
+    }
+    
+    
+}
--- CryptoViper/View.swift
+++ CryptoViper/View.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import UIKit
+
+protocol AnyView{
+    
+}
+
+class CryptoViewController : UIViewController, AnyView{
+    
+}
