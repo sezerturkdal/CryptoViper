8449585ebe57a7c6eaa7483da39a5f5082c3874f
Sezer Turkdal <sezer.turkdal@hotmail.com>
2023-01-12 22:34:21 +0300

    View and router edited

--- CryptoViper/Router.swift
+++ CryptoViper/Router.swift
@@ -6,14 +6,35 @@
 //
 
 import Foundation
+import UIKit
+
+typealias EntryPoint = AnyView & UIViewController
 
 protocol AnyRouter{
-    static func startExecution() -> AnyObject
+    var entry : EntryPoint? {get}
+    static func startExecution() -> AnyRouter
 }
 
 class CryptoRouter : AnyRouter {
-    static func startExecution() -> AnyObject {
+    var entry : EntryPoint?
+    
+    static func startExecution() -> AnyRouter {
         let router = CryptoRouter()
+        
+        var view : AnyView = CryptoViewController()
+        var presenter : AnyPresenter = CryptoPresenter()
+        var interactor : AnyInteractor = CryptoInteractor()
+        
+        view.presenter = presenter
+        
+        presenter.view = view
+        presenter.router = router
+        presenter.interactor = interactor
+        
+        interactor.presenter = presenter
+        
+        router.entry = view as? EntryPoint
+        
         return router
     }
     
--- CryptoViper/View.swift
+++ CryptoViper/View.swift
@@ -9,9 +9,74 @@ import Foundation
 import UIKit
 
 protocol AnyView{
+    var presenter : AnyPresenter?{get set}
     
+    func update(with cryptos:[Crypto])
+    func update(with error:String)
 }
 
-class CryptoViewController : UIViewController, AnyView{
+class CryptoViewController : UIViewController, AnyView, UITableViewDelegate, UITableViewDataSource{
+    
+    var presenter: AnyPresenter?
+    var cryptos : [Crypto] = []
+    
+    private let tableView : UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.isHidden=true
+        return table
+    }()
+    
+    private let messageLabel : UILabel = {
+        let label = UILabel()
+        label.isHidden = false
+        label.text = "Downloading..."
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textColor = UIColor.blue
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+        view.addSubview(tableView)
+        view.addSubview(messageLabel)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cryptos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tableView.frame = view.bounds
+        messageLabel.frame = CGRect(x: view.bounds.width / 2 - 100, y: view.bounds.height / 2 - 25, width: 200, height: 50)
+        
+    }
+    
+    func update(with cryptos: [Crypto]) {
+        DispatchQueue.main.async {
+            self.cryptos = cryptos
+            self.messageLabel.isHidden = false
+            self.tableView.reloadData()
+            self.tableView.isHidden = true
+        }
+    }
+    
+    func update(with error: String) {
+        
+    }
+    
     
 }
